902ba66d6f1221f5da41d9538613310ccb14c635
young <zerobugpark@gmail.com>
2025-01-11 00:58:28 +0900

    채팅 테이블뷰 셀 추가

---
+++ TravelTalk/OtherUserTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  OtherUserTableViewCell.swift
+//  TravelTalk
+//
+//  Created by youngkyun park on 1/11/25.
+//
+
+import UIKit
+
+class OtherUserTableViewCell: UITableViewCell {
+
+    
+    static let identifier = "OtherUserTableViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+      
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    
+    }
+    
+}
