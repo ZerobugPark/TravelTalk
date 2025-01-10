445516df623d8fbe9ee7046d4deb99cc1676e029
young <zerobugpark@gmail.com>
2025-01-11 00:24:35 +0900

    스토리보드 뷰컨트롤러 추가

--- TravelTalk/ChatViewController.swift
+++ TravelTalk/ChatViewController.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class ChatViewController: UIViewController {
 
+    
+    static let identifier = "ChatViewController"
+    
+    var chat:[Chat] = []
+    var navigationTitle = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        print(chat)
+       
+        
+        self.navigationItem.title = navigationTitle
     }
     
 
-    /*
-    // MARK: - Navigation
+    
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
