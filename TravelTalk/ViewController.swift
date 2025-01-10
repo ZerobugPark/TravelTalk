9f0b775672e0d7bfb6a147b718a86cc6d684d241
young <zerobugpark@gmail.com>
2025-01-10 22:50:23 +0900

    델리게이트 추가 및 기본 코드 작성

--- TravelTalk/ViewController.swift
+++ TravelTalk/ViewController.swift
@@ -9,11 +9,153 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var travelTalk = mockChatList
+    var charArray: [String] = []
+    
+    @IBOutlet var chatCollectionView: UICollectionView!
+    
+    @IBOutlet var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        chatCollectionView.delegate = self
+        chatCollectionView.dataSource = self
+        searchBar.delegate = self
+        
+        
+        setup()
+        
+        configureCollectionView()
+        configureChatCollectionViewLayout()
+        
+    }
+    
+    private func setup() {
+        let placeholder = "찾고 싶은 사용자를 입력해주세요."
+        searchBar.placeholder = placeholder
+        searchBar.showsCancelButton = true
+    }
+    
+    
+    private func configureCollectionView() {
+        
+        
+        var xib = UINib(nibName: ChatListCollectionViewCell.identifier, bundle: nil)
+        chatCollectionView.register(xib, forCellWithReuseIdentifier: ChatListCollectionViewCell.identifier)
+        
+        xib = UINib(nibName: MultiChatListCollectionViewCell.identifier, bundle: nil)
+        chatCollectionView.register(xib, forCellWithReuseIdentifier: MultiChatListCollectionViewCell.identifier)
+    }
+    
+    private func configureChatCollectionViewLayout() {
+        
+        let layout = UICollectionViewFlowLayout()
+
+        //let deviceWidth = UIScreen.main.bounds.width
+        layout.scrollDirection = .vertical
+        
+        let cellWidth = UIScreen.main.bounds.width
+        layout.itemSize = CGSize(width: cellWidth, height: 70)
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 16, bottom: 16, right: 10)
+        
+        
+        chatCollectionView.collectionViewLayout = layout
+        
+        
+        
     }
 
 
 }
 
+// MARK: - CollectionView Delegate
+
+extension ViewController: UICollectionViewDelegate,UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        travelTalk.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if travelTalk[indexPath.item].chatroomImage.count > 1 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MultiChatListCollectionViewCell.identifier, for: indexPath) as! MultiChatListCollectionViewCell
+            
+            cell.configMultiData(chatInfo: travelTalk, item: indexPath.item)
+            
+            for i in 0..<cell.userImageViews.count {
+                DispatchQueue.main.async {
+                    cell.userImageViews[i].layer.cornerRadius = 10
+                }
+            }
+            return cell
+            
+        } else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ChatListCollectionViewCell.identifier, for: indexPath) as! ChatListCollectionViewCell
+            
+            cell.configData(chatInfo: travelTalk, item: indexPath.item)
+            
+            DispatchQueue.main.async {
+                cell.userImageView.layer.cornerRadius = cell.userImageView.frame.width / 2
+            }
+        
+            
+            return cell
+        }
+
+            
+    }
+    
+    private func searchChatRoom(charArray: [String]) {
+        
+        let str = charArray.joined().uppercased()
+        
+        
+        //let a = mockChatList.filter { $0.chatroomName.contains(str) }
+        //print(str)
+        if str.isEmpty {
+            travelTalk = mockChatList
+            print("here")
+        } else {
+            travelTalk = mockChatList.filter { $0.chatroomName.uppercased().contains(str) }
+        }
+        chatCollectionView.reloadData()
+        
+    }
+    
+    
+}
+
+// MARK: - SearchBar Delegate
+
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+  
+  
+        if !text.isEmpty {
+            charArray.append(text)
+            print(text)
+        } else {
+            if !charArray.isEmpty {
+                charArray.removeLast()
+            }
+        }
+
+        searchChatRoom(charArray: charArray)
+        return true
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+    }
+    
+
+    
+}
