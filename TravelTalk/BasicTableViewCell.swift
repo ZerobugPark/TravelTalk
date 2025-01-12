9f6c20f0c8a2f41d36265b427b725d052b1d869d
young <zerobugpark@gmail.com>
2025-01-12 17:15:49 +0900

    cell 리턴 방지용 기본 셀 구현

---
+++ TravelTalk/BasicTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  BasicTableViewCell.swift
+//  TravelTalk
+//
+//  Created by youngkyun park on 1/12/25.
+//
+
+import UIKit
+
+class BasicTableViewCell: UITableViewCell {
+
+    
+    static let identifier = "BasicTableViewCell"
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
